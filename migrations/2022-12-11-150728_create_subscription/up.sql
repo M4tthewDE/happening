0c17d4bdfd3e249da2a03fbe0f805ea81d7b53dd
M4tthewDE <mattider2@gmail.com>
2022-12-13 16:08:12 +0100

    Remove rust backend

--- migrations/2022-12-11-150728_create_subscription/up.sql
+++
@@ -1,8 +0,0 @@
--- Your SQL goes here
-
-CREATE TABLE subscription (
-    subscription_id SERIAL PRIMARY KEY,
-    target_id VARCHAR NOT NULL,
-    subscription_type VARCHAR NOT NULL,
-    eventsub_id VARCHAR UNIQUE NOT NULL
-)
