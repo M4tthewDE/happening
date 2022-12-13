1f14134673e97e69b7f92842afa2d4a1ea7f5917
M4tthewDE <mattider2@gmail.com>
2022-12-13 18:40:39 +0100

    Adjust lambda to work with api gateway

--- backend/api/main.go
+++ backend/api/main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-	"context"
-	"fmt"
+	"log"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
-}
-
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
+func HandleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Printf("Processing Lambda request %t\n", request.RequestContext)
+	return events.APIGatewayProxyResponse{
+		Body:       "Hello world",
+		StatusCode: 200,
+	}, nil
 }
 
 func main() {
+	log.Printf("Start lambda")
 	lambda.Start(HandleRequest)
 }
