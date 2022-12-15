3bfde0ed92062d9b289d31ab6fbfaf7fbba2ceee
M4tthewDE <mattider2@gmail.com>
2022-12-15 20:42:35 +0100

    Create acm certificate

--- dns.tf
+++ dns.tf
@@ -1,6 +1,21 @@
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "happening.fdm.com.de"
+  validation_method = "DNS"
+
+  tags = {
+    Environment = "develop"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 data "cloudflare_zone" "zone" {
   name = "fdm.com.de"
 }
+
+/*
 
 resource "cloudflare_record" "happening" {
   zone_id = data.cloudflare_zone.zone.zone_id
@@ -9,6 +24,7 @@ resource "cloudflare_record" "happening" {
 
   proxied = true # Take advantage of Cloudflare http caching
 }
+*/
 
 // https://blog.viktoradam.net/2018/08/30/moving-home/
 
