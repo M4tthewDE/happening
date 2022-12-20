9b48401abd56d0ccf3fde3fdf82c30c632376a9d
M4tthewDE <mattider2@gmail.com>
2022-12-20 14:48:48 +0100

    Handle specific events separately

--- backend/api/internal/twitch.go
+++ backend/api/internal/twitch.go
@@ -1,6 +1,9 @@
 package internal
 
 import (
+	"bytes"
+	"encoding/json"
+	"log"
 	"os"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -9,7 +12,6 @@ import (
 
 func HandleNewEventsubEvent(request events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
 	headers := make(map[string][]string)
-
 	for key, value := range request.Headers {
 		v := make([]string, 1)
 		v = append(v, value)
@@ -22,6 +24,50 @@ func HandleNewEventsubEvent(request events.APIGatewayProxyRequest) events.APIGat
 			StatusCode: 403,
 		}
 	}
+
+	var vals eventSubNotification
+	err := json.NewDecoder(bytes.NewReader([]byte(request.Body))).Decode(&vals)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "",
+			StatusCode: 400,
+		}
+	}
+
+	if vals.Challenge != "" {
+		return events.APIGatewayProxyResponse{
+			Body:       vals.Challenge,
+			StatusCode: 200,
+		}
+	}
+	switch request.Path {
+	case "/api/twitch/follow":
+		return handleFollowEvent(request)
+	default:
+		return events.APIGatewayProxyResponse{
+			Body:       "",
+			StatusCode: 404,
+		}
+	}
+}
+
+type eventSubNotification struct {
+	Subscription helix.EventSubSubscription `json:"subscription"`
+	Challenge    string                     `json:"challenge"`
+	Event        json.RawMessage            `json:"event"`
+}
+
+func handleFollowEvent(request events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
+	var followEvent helix.EventSubChannelFollowEvent
+	err := json.NewDecoder(bytes.NewReader([]byte(request.Body))).Decode(&followEvent)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body:       "",
+			StatusCode: 400,
+		}
+	}
+
+	log.Println(followEvent)
 
 	return events.APIGatewayProxyResponse{
 		Body:       "",
