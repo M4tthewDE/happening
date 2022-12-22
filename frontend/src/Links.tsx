15cc8d82bba1114a23f36e8c00a5758f533602af
M4tthewDE <mattider2@gmail.com>
2022-12-22 15:51:01 +0100

    Add links section to navbar

---
+++ frontend/src/Links.tsx
@@ -0,0 +1,28 @@
+import { Group, Text, UnstyledButton } from "@mantine/core";
+import { IconNotification } from "@tabler/icons";
+
+function Links() {
+    return (
+        <UnstyledButton
+            sx={(theme) => ({
+                display: 'block',
+                width: '100%',
+                padding: theme.spacing.xs,
+                borderRadius: theme.radius.sm,
+                color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+
+                '&:hover': {
+                    backgroundColor:
+                        theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+                },
+            })}
+        >
+            <Group>
+                <IconNotification></IconNotification>
+                <Text>Eventsub</Text>
+            </Group>
+        </UnstyledButton>
+    );
+}
+
+export default Links;
