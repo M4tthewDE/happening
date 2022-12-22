2ab312e1ce85cc1455c15a3a31655ebcdc22b950
M4tthewDE <mattider2@gmail.com>
2022-12-22 16:31:51 +0100

    Extract Navbarlink into component

--- frontend/src/Links.tsx
+++ frontend/src/Links.tsx
@@ -1,57 +1,12 @@
-import { Group, Text, ThemeIcon, UnstyledButton } from "@mantine/core";
 import { IconHome, IconTimelineEvent } from "@tabler/icons";
-import { Link } from "react-router-dom";
+import NavbarLink from "./components/NavbarLink";
 
 function Links() {
 
     return (
         <div>
-            <Link to={"/"} style={{ textDecoration: 'none' }}>
-                <UnstyledButton
-                    sx={(theme) => ({
-                        display: 'block',
-                        width: '100%',
-                        padding: theme.spacing.xs,
-                        borderRadius: theme.radius.sm,
-                        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
-
-                        '&:hover': {
-                            backgroundColor:
-                                theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-                        },
-                    })}
-                >
-                    <Group>
-                        <ThemeIcon color="green" variant="light">
-                            <IconHome></IconHome>
-                        </ThemeIcon>
-                        <Text>Home</Text>
-                    </Group>
-                </UnstyledButton >
-            </Link>
-            <Link to={"/eventsub"} style={{ textDecoration: 'none' }}>
-                <UnstyledButton
-                    sx={(theme) => ({
-                        display: 'block',
-                        width: '100%',
-                        padding: theme.spacing.xs,
-                        borderRadius: theme.radius.sm,
-                        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
-
-                        '&:hover': {
-                            backgroundColor:
-                                theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-                        },
-                    })}
-                >
-                    <Group>
-                        <ThemeIcon color="blue" variant="light">
-                            <IconTimelineEvent></IconTimelineEvent>
-                        </ThemeIcon>
-                        <Text>Eventsub</Text>
-                    </Group >
-                </UnstyledButton >
-            </Link>
+            <NavbarLink route={"/"} text={"Home"} icon={<IconHome />} icon_color={"green"} />
+            <NavbarLink route={"/eventsub"} text={"Eventsub"} icon={<IconTimelineEvent />} icon_color={"blue"} />
         </div>
     );
 }
