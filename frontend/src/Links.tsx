0b44e45f2b3d753cb82c33fc1712a75615685cce
M4tthewDE <mattider2@gmail.com>
2022-12-22 16:24:17 +0100

    Add routing

--- frontend/src/Links.tsx
+++ frontend/src/Links.tsx
@@ -1,29 +1,58 @@
-import { Group, Text, ThemeIcon, UnstyledButton } from "@mantine/core";
-import { IconTimelineEvent } from "@tabler/icons";
+import { Group, Space, Text, ThemeIcon, UnstyledButton } from "@mantine/core";
+import { IconHome, IconTimelineEvent } from "@tabler/icons";
+import { Link } from "react-router-dom";
 
 function Links() {
-    return (
-        <UnstyledButton
-            sx={(theme) => ({
-                display: 'block',
-                width: '100%',
-                padding: theme.spacing.xs,
-                borderRadius: theme.radius.sm,
-                color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
 
-                '&:hover': {
-                    backgroundColor:
-                        theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-                },
-            })}
-        >
-            <Group>
-                <ThemeIcon color="blue" variant="light">
-                    <IconTimelineEvent></IconTimelineEvent>
-                </ThemeIcon>
-                <Text>Eventsub</Text>
-            </Group>
-        </UnstyledButton>
+    return (
+        <div>
+            <Link to={"/"}>
+                <UnstyledButton
+                    sx={(theme) => ({
+                        display: 'block',
+                        width: '100%',
+                        padding: theme.spacing.xs,
+                        borderRadius: theme.radius.sm,
+                        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+
+                        '&:hover': {
+                            backgroundColor:
+                                theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+                        },
+                    })}
+                >
+                    <Group>
+                        <ThemeIcon color="green" variant="light">
+                            <IconHome></IconHome>
+                        </ThemeIcon>
+                        <Text>Home</Text>
+                    </Group>
+                </UnstyledButton >
+            </Link>
+            <Link to={"/eventsub"}>
+                <UnstyledButton
+                    sx={(theme) => ({
+                        display: 'block',
+                        width: '100%',
+                        padding: theme.spacing.xs,
+                        borderRadius: theme.radius.sm,
+                        color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+
+                        '&:hover': {
+                            backgroundColor:
+                                theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+                        },
+                    })}
+                >
+                    <Group>
+                        <ThemeIcon color="blue" variant="light">
+                            <IconTimelineEvent></IconTimelineEvent>
+                        </ThemeIcon>
+                        <Text>Eventsub</Text>
+                    </Group >
+                </UnstyledButton >
+            </Link>
+        </div>
     );
 }
 
