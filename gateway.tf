91a1ded211f3978a0e8d2149ebbb1952f2d37689
M4tthewDE <mattider2@gmail.com>
2022-12-23 15:34:24 +0100

    Move dns out of gateway definition

--- gateway.tf
+++ gateway.tf
@@ -63,18 +63,10 @@ resource "aws_lambda_permission" "lambda_permission" {
   source_arn    = "${aws_api_gateway_deployment.api_deployment.execution_arn}/*/*"
 }
 
-resource "aws_acm_certificate" "cert" {
-  domain_name       = "${local.sub_domain}.fdm.com.de"
-  validation_method = "DNS"
-
-  tags = {
-    Environment = "develop"
-  }
-}
-
-// https://blog.viktoradam.net/2018/08/30/moving-home/
-resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn = aws_acm_certificate.cert.arn
+resource "aws_api_gateway_base_path_mapping" "mapping" {
+  api_id      = aws_api_gateway_rest_api.api.id
+  stage_name  = aws_api_gateway_deployment.api_deployment.stage_name
+  domain_name = aws_api_gateway_domain_name.happening.domain_name
 }
 
 resource "aws_api_gateway_domain_name" "happening" {
@@ -84,39 +76,4 @@ resource "aws_api_gateway_domain_name" "happening" {
   endpoint_configuration {
     types = ["REGIONAL"]
   }
-}
-
-resource "aws_api_gateway_base_path_mapping" "mapping" {
-  api_id      = aws_api_gateway_rest_api.api.id
-  stage_name  = aws_api_gateway_deployment.api_deployment.stage_name
-  domain_name = aws_api_gateway_domain_name.happening.domain_name
-}
-
-data "cloudflare_zone" "zone" {
-  name = "fdm.com.de"
-}
-
-resource "cloudflare_record" "verification" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-    }
-  }
-
-  zone_id = data.cloudflare_zone.zone.zone_id
-  name    = each.value.name
-  value   = each.value.record
-  type    = "CNAME"
-
-  proxied = false # Take advantage of Cloudflare http caching
-}
-
-resource "cloudflare_record" "happening" {
-  zone_id = data.cloudflare_zone.zone.zone_id
-  name    = local.sub_domain
-  type    = "CNAME"
-  value   = aws_api_gateway_domain_name.happening.regional_domain_name
-
-  proxied = true # Take advantage of Cloudflare http caching
 }
