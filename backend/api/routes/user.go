f771309986a1df63cdbdc418c3923535ab52b14a
M4tthewDE <mattider2@gmail.com>
2022-12-26 11:05:55 +0100

    Refactor backend

--- backend/api/routes/user.go
+++ backend/api/routes/user.go
@@ -1,47 +1,15 @@
 package routes
 
 import (
-	"context"
 	"encoding/json"
 	"log"
-	"os"
 
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/m4tthewde/happening/backend/api/internal"
 	"github.com/nicklaw5/helix"
 )
 
 func GetUser(request events.APIGatewayProxyRequest) (string, int) {
-	ctx := context.TODO()
-	cfg, err := config.LoadDefaultConfig(ctx, func(o *config.LoadOptions) error {
-		o.Region = "us-east-1"
-		return nil
-	})
-	if err != nil {
-		log.Println(err)
-		return "", 500
-	}
-
-	dbClient := dynamodb.NewFromConfig(cfg)
-	d := internal.NewDao(dbClient)
-
-	token, err := d.GetAuth(ctx)
-	if err != nil {
-		log.Println(err)
-		return "", 500
-	}
-
-	client, err := helix.NewClient(&helix.Options{
-		ClientID:       os.Getenv("TWITCH_CLIENT_ID"),
-		AppAccessToken: token,
-	})
-	if err != nil {
-		log.Println(err)
-		return "", 500
-	}
-
 	var params helix.UsersParams
 	name, ok := request.QueryStringParameters["name"]
 	if ok {
@@ -55,7 +23,7 @@ func GetUser(request events.APIGatewayProxyRequest) (string, int) {
 		params = helix.UsersParams{IDs: []string{id}}
 	}
 
-	resp, err := client.GetUsers(&params)
+	resp, err := internal.GetUsers(&params)
 	if err != nil {
 		log.Println(err)
 		return "", 500
