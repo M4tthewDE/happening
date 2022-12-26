967406be2b386658e9d83e8076c491f0ce19037c
M4tthewDE <mattider2@gmail.com>
2022-12-26 11:08:37 +0100

    Add method to create twitch client

--- backend/api/internal/twitch.go
+++ backend/api/internal/twitch.go
@@ -7,6 +7,18 @@ import (
 	"github.com/nicklaw5/helix"
 )
 
+func getClient() (*helix.Client, error) {
+	token, err := GetAuth(context.TODO())
+	if err != nil {
+		return nil, err
+	}
+
+	return helix.NewClient(&helix.Options{
+		ClientID:       os.Getenv("TWITCH_CLIENT_ID"),
+		AppAccessToken: token,
+	})
+}
+
 func ValidateToken(token string) (bool, *helix.ValidateTokenResponse, error) {
 	client, err := helix.NewClient(&helix.Options{
 		ClientID:       os.Getenv("TWITCH_CLIENT_ID"),
@@ -20,15 +32,7 @@ func ValidateToken(token string) (bool, *helix.ValidateTokenResponse, error) {
 }
 
 func GetUsers(params *helix.UsersParams) (*helix.UsersResponse, error) {
-	token, err := GetAuth(context.TODO())
-	if err != nil {
-		return nil, err
-	}
-
-	client, err := helix.NewClient(&helix.Options{
-		ClientID:       os.Getenv("TWITCH_CLIENT_ID"),
-		AppAccessToken: token,
-	})
+	client, err := getClient()
 	if err != nil {
 		return nil, err
 	}
@@ -37,15 +41,7 @@ func GetUsers(params *helix.UsersParams) (*helix.UsersResponse, error) {
 }
 
 func RemoveEventSubSubscription(id string) (*helix.RemoveEventSubSubscriptionParamsResponse, error) {
-	token, err := GetAuth(context.TODO())
-	if err != nil {
-		return nil, err
-	}
-
-	client, err := helix.NewClient(&helix.Options{
-		ClientID:       os.Getenv("TWITCH_CLIENT_ID"),
-		AppAccessToken: token,
-	})
+	client, err := getClient()
 	if err != nil {
 		return nil, err
 	}
@@ -54,15 +50,7 @@ func RemoveEventSubSubscription(id string) (*helix.RemoveEventSubSubscriptionPar
 }
 
 func GetEventSubSubscriptions() (*helix.EventSubSubscriptionsResponse, error) {
-	token, err := GetAuth(context.TODO())
-	if err != nil {
-		return nil, err
-	}
-
-	client, err := helix.NewClient(&helix.Options{
-		ClientID:       os.Getenv("TWITCH_CLIENT_ID"),
-		AppAccessToken: token,
-	})
+	client, err := getClient()
 	if err != nil {
 		return nil, err
 	}
@@ -71,15 +59,7 @@ func GetEventSubSubscriptions() (*helix.EventSubSubscriptionsResponse, error) {
 }
 
 func CreateEventSubSubscription(sub *helix.EventSubSubscription) (*helix.EventSubSubscriptionsResponse, error) {
-	token, err := GetAuth(context.TODO())
-	if err != nil {
-		return nil, err
-	}
-
-	client, err := helix.NewClient(&helix.Options{
-		ClientID:       os.Getenv("TWITCH_CLIENT_ID"),
-		AppAccessToken: token,
-	})
+	client, err := getClient()
 	if err != nil {
 		return nil, err
 	}
